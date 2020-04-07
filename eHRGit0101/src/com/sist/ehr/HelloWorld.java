80a3e1e772ec8e2686333c84b20d040e009bc7c6
sist <sist@DESKTOP-QK2CKFJ>
2020-04-07 10:20:11 +0900

    커밋커밋~~

--- eHRGit0101/src/com/sist/ehr/HelloWorld.java
+++ eHRGit0101/src/com/sist/ehr/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, Git!");
 		System.out.println("Hello, Git~~");
+		System.out.println("내가 커밋을 해볼게^^");
 
 	}
 
